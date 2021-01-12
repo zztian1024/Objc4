0a50722b31f5c41dcb6b1040df59b045087eee31
zhenzi <tianzhenzi@inke.cn>
2021-01-12 21:03:41 +0800

    Build success

---
+++ objc4-818.2/ObjcDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcDemo
+//
+//  Created by Tian on 2021/1/12.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
